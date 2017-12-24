82a01494acc97746563b40f157e0738832fb8e4b
John Gardner <jeg1972@gmail.com>
2017-12-24 19:28:00 +0000

    FIX: Set UK Timezone

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
+    currentTime.setTimezone("Europe/London");
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
